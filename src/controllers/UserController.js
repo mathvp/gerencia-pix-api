ea9a1321994f029b0b0bb54b315b662c27441493
Matheus Vieira <matheusvieiradepaula@gmail.com>
2021-11-06 03:00:30 -0300

    Update Token expire time

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -72,8 +72,7 @@ module.exports = {
         }
 
         const token = jwt.sign({ id: user.id }, `${config.secret}`, {
-          //expiresIn: 86400 // 24 hours
-          expiresIn: 60
+          expiresIn: 86400 // 24 hours
         });
 
         return res.status(200).json({
